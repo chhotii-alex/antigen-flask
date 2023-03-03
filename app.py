c635cfc6108cbc5afd7848a9949d88df49f56463
Alex Morgan <amorgan2@bidmc.harvard.edu>
2023-03-03 11:03:52 -0500

    testing what happens after crash

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_from_directory
 
+import sys
+
 app = Flask(__name__,
             static_url_path='',
             static_folder='static')
@@ -26,3 +28,7 @@ def assays():
     }
     return data
 
+@app.route("/kill")
+def killer():
+    print("Site going down")
+    sys.exit(1)
