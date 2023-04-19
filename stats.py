81ff89f7f9147cb3a610bf4ba24bc1216f9b9098
Alex Morgan <amorgan2@bidmc.harvard.edu>
2023-04-19 15:58:19 -0400

    something we want to get back to

--- stats.py
+++ stats.py
@@ -1,6 +1,11 @@
 from scipy.stats import kstest
+#from scipy.stats import wasserstein_distance
 
 # Kolmogorov-Smirnov test comparing distributions
-def compare(list_a, list_b):
+def compare(list_a, list_b, label_a="", label_b=""):
     res = kstest(list_a, list_b)
-    return res.pvalue
+    ks_pvalue =  res.pvalue
+    #distance = wasserstein_distance(list_a, list_b)
+    #n = len(list_a) + len(list_b)
+    #print("%f\t%f\t%f\t%s\t%s" % (ks_pvalue, distance, n, label_a, label_b))
+    return ks_pvalue
